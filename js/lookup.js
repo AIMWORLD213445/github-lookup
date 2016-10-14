85b6500f284ab165afc17734a59d7f905e129f4a
Brian Dutz <bgdutz@gmail.com>
2016-10-14 16:19:47 -0700

    final commit and tweaks

--- js/lookup.js
+++ js/lookup.js
@@ -4,7 +4,7 @@ function Lookup() {
 }
 
 Lookup.prototype.getRepos = function(username, displayFunction) {
-  $.get('https://api.github.com/users/' + username + '/repos?access_token=' + apiKey).then(function(response){
+  $.get('https://api.github.com/users/' + username + '/repos?per_page=100>access_token=' + apiKey).then(function(response){
 
 for (var i = 0; i<response.length; i++) {
     console.log(response[i].name);
