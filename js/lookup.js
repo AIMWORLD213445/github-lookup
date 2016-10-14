a7ed440956b170361adbd320bdcde188ed4ffecd
Brian Dutz <bgdutz@gmail.com>
2016-10-14 10:18:00 -0700

    alter API query to gather repo info rather than broader user info

--- js/lookup.js
+++ js/lookup.js
@@ -4,9 +4,11 @@ function Lookup() {
 }
 
 Lookup.prototype.getRepos = function(userName, displayFunction){
-  $.get('https://api.github.com/users/' + userName + '?access_token=' + apiKey).then(function(response){
+  $.get('https://api.github.com/users/' + userName + '/repos?access_token=' + apiKey).then(function(response){
     displayFunction(response);
   }).fail(function(error){
     console.log(error.responseJSON.message);
   });
 };
+
+exports.lookupModule = Lookup;
