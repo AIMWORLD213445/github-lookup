dee5c313a31b04e38be85c3808ae53a43f2702b4
Brian Dutz <bgdutz@gmail.com>
2016-10-14 09:51:36 -0700

    start work on back end prototype

---
+++ js/lookup.js
@@ -0,0 +1,12 @@
+var apiKey = require('./../.env').apiKey;
+
+function Lookup() {
+}
+
+Lookup.prototype.getRepos = function(userName, displayFunction){
+  $.get('https://api.github.com/users/' + userName + '?access_token=' + apiKey).then(function(response){
+    displayFunction(response);
+  }).fail(function(error){
+    console.log(error.responseJSON.message);
+  });
+};
